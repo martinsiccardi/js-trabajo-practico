ccdaea3e68db8162bb1bef3d0d23b726f62c98ac
martinsiccardi <fmsiccardi@escuelasproa.edu.ar>
2023-05-31 09:46:03 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function sumar()  {
+    var numero1 = document.getElementById("numero1").value;
+    var numero2 = document.getElementById("numero2").value;
+
+    var suma = parseInt(numero1) + parseInt(numero2)
+
+    document.writeln(suma);
+}
