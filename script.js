2bd4fc42698019da494fc831b420bf5a7710f4c7
martinsiccardi <fmsiccardi@escuelasproa.edu.ar>
2023-06-01 09:34:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,30 +8,4 @@ function sumar()  {
     document.writeln(suma);
 }
 
-function restar()  {
-    var numero1 = document.getElementById("numero1").value;
-    var numero2 = document.getElementById("numero2").value;
-
-    var resta = parseInt(numero1) - parseInt(numero2)
-
-    document.writeln(resta);
-}
-
-function multiplicar()  {
-    var numero1 = document.getElementById("numero1").value;
-    var numero2 = document.getElementById("numero2").value;
-
-    var multiplicacion = parseInt(numero1) * parseInt(numero2)
-
-    document.writeln(multiplicacion);
-}
-
-function division()  {
-    var numero1 = document.getElementById("numero1").value;
-    var numero2 = document.getElementById("numero2").value;
-
-    var dividir = parseInt(numero1) / parseInt(numero2)
-
-    document.writeln(dividir);
-}
 
